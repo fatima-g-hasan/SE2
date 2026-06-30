5b6995296c53ec041df436c163ee6e5cbf544f02
thecoder <fathas235@gmail.com>
2026-06-30 19:49:49 +0300

    Fix dotenv path for build environment

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { DBMode } from "./types";
 import { StringValue } from "ms";
 
-dotenv.config({ path: path.join(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 export default {
   logDir: process.env.LOG_DIR || "./logs",
