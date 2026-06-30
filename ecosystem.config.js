4f46b57eea53b5677cc2b53e42cad3b276fc6d39
thecoder <fathas235@gmail.com>
2026-06-30 21:03:03 +0300

    added pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'se2',
+  script: '/build/src/index.js',
+  exec_mode: 'fork',
+  instances: 1,
+  env: {
+    NODE_ENV: 'development'
+  },
+  env_production: {
+    NODE_ENV: 'production'
+  },
+  env_stage: {
+    NODE_ENV: 'stage'
+  }
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { DBMode } from "./types";
 import { StringValue } from "ms";
 
-dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+dotenv.config({ path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`) });
 
 export default {
   logDir: process.env.LOG_DIR || "./logs",
